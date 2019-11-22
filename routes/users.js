2484464c8ee1ca84678ed919d5e99e258cdd05ab
Erik Ruhmanis <erik2310@edu.easj.dk>
2019-11-22 19:40:19 +0100

    Har fået createuser POST til at virke.

--- routes/users.js
+++ routes/users.js
@@ -1,9 +1,35 @@
 var express = require('express');
 var router = express.Router();
+var MongoClient = require('mongodb').MongoClient;
+var url = "mongodb://localhost:27017/";
 
-/* GET users listing. */
-router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
+
+/* GET til at teste om /api/users virker. */
+router.get('/', function (req, res, next) {
+  res.send('Den virker!');
 });
+
+/* POST til at oprette en bruger */
+router.post('/createuser', function (req, res, next) {
+
+  MongoClient.connect(url, function (err, db) {
+    if (err) throw err;
+    var dbo = db.db("deleVennerDB");
+
+    var object = {
+      fullname: req.body.fullname,
+      email: req.body.email,
+      password: req.body.password
+    };
+
+    dbo.collection("users").insertOne(object, function (err, res) {
+      if (err) throw err;
+      console.log("1 document inserted");
+      db.close();
+    });
+  });
+  res.sendStatus(200);
+
+})
 
 module.exports = router;
